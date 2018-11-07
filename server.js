774585698f14a361503036789ae87390596d74fd
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-07 16:21:59 +0800

    Added some libs and also added api proxy for reddit

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var fetch = require('node-fetch');
 var app = express();
 var port = 3030;
 
 app.get('/' , (request,response) => {
-    response.send('ehy there!')
+    response.send(`/api/reddit/:subreddit/:limit`)
+})
+
+// REDDIT
+app.get('/api/reddit/:subreddit/:limit?' , (request,response) => {
+    var subreddit = request.params.subreddit;
+    var limit = request.params.limit;
+
+    fetch('https://www.reddit.com/r/' + subreddit + '/new.json?limit=' + limit)
+    .then(response => response.json())
+    .then(json => {
+        response.send(json)
+    })
+
 })
 
 app.listen(port , () => {
